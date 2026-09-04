0c26faf73800ac9c1f9b56e5696221678983bf63
andeng07 <sumilangandreijohn@gmail.com>
2026-09-04 22:42:51 +0800

    feat: project setup

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,3 @@
+<header class="w-full bg-cream h-16 px-6 flex flex-row justify-center items-center">
+    test
+</header>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import logo from "$lib/assets/logo-v2.svg";
+    import Header from "$lib/components/Header.svelte";
+    import "../app.css";
+
+    let { children } = $props();
+</script>
+
+<svelte:head>
+    <link rel="icon" href={logo} />
+    <title>etheralinks</title>
+</svelte:head>
+
+<div class="font-nohemi font-normal">
+    <Header/>
+    {@render children()}
+</div>
