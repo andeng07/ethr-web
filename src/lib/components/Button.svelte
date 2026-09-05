4eeaf0ab2bc5c98a268b1cf58b14038590bd66e4
andeng07 <sumilangandreijohn@gmail.com>
2026-09-06 00:13:57 +0800

    feat: add Button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    let { href, label }: { href: string; label: string } = $props();
+</script>
+
+<div>
+    <a {href} class="border-b border-b-muted text-md text-muted hover:border-b-blue hover:text-blue">
+        {label}
+    </a>
+</div>
