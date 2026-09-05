5af566a9e0be3c4152c736bc9d918f8c48ca2dfd
andeng07 <sumilangandreijohn@gmail.com>
2026-09-06 03:31:08 +0800

    refactor: lessen text size when for screen width < md

--- src/routes/AboutUsSection.svelte
+++ src/routes/AboutUsSection.svelte
@@ -9,7 +9,7 @@
 <section class="py-18 px-6">
     <div class="w-full flex flex-col justify-center items-center gap-24">
         <img src={logo} alt="logo" class="h-16" />
-        <h1 class="text-5xl text-center">always & forever</h1>
+        <h1 class="text-4xl text-center md:text-5xl">always & forever</h1>
         <Button href={aboutUsLink} label="ABOUT US"/>
     </div>
 </section>
